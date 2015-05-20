75f5c627ed18a693d24813b976f3731c8c188852
Randy Zwitch <rzwitch@gmail.com>
2015-05-20 09:07:13 -0400

    Consolidate tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,21 @@
 using Vega
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+my_tests = ["test/padding.jl",
+            "test/valueref.jl",
+            "test/axis.jl",
+            "test/format.jl",
+            "test/transform.jl",
+            "test/data.jl",
+            "test/mark.jl",
+            "test/scale.jl",
+            "test/scale.jl",
+            "test/visualization.jl",
+            "demo.jl"]
+
+println("Running tests:")
+
+for my_test in my_tests
+    println(" * $(my_test)")
+    include(Pkg.dir("Vega",my_test))
+end
