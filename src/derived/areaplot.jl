41d68862f18b55d465973edfc62ac4c418a18e22
Randy Zwitch <rzwitch@gmail.com>
2015-06-03 14:52:50 -0400

    Add stacked area plot
    Pretty much same as stacked bar, except using area

--- src/derived/areaplot.jl
+++ src/derived/areaplot.jl
@@ -1,6 +1,7 @@
 function areaplot(;x::AbstractVector = Int[],
                    y::AbstractVector = Int[],
-                   group::AbstractVector = Int[])
+                   group::AbstractVector = Int[],
+                   stacked::Bool = false)
 
     v = barplot(x = x, y = y, group = group)
 
@@ -9,6 +10,15 @@ function areaplot(;x::AbstractVector = Int[],
     v.marks[1].properties.enter.interpolate = VegaValueRef(value = "monotone")
     v.scales[1]._type = nothing
     v.scales[1].zero = false
+
+    if stacked
+    	v = barplot(x = x, y = y, group = group, stacked = true)
+	    v.scales[1].zero = false
+		v.scales[1]._type = nothing
+		v.marks[1].marks[1]._type = "area"
+		v.marks[1].marks[1].properties.enter.interpolate = VegaValueRef(value = "monotone")
+		v.marks[1].marks[1].properties.enter.width = nothing
+	end
 
     return v
 end
