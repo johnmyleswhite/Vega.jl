5cbc8692342aa9b30a17fb4df21e9b9780b89896
Randy Zwitch <rzwitch@gmail.com>
2015-06-03 12:59:15 -0400

    Add stacked bar, refine area plot
    Add stacked bar chart as a keyword on barplot(), refactored area plot
    to use bar plot as the base.

--- src/derived/areaplot.jl
+++ src/derived/areaplot.jl
@@ -1,18 +1,14 @@
 function areaplot(;x::AbstractVector = Int[],
                    y::AbstractVector = Int[],
                    group::AbstractVector = Int[])
-    v = VegaVisualization()
 
-    default_scales!(v)
-    default_axes!(v)
-    default_legend!(v)
+    v = barplot(x = x, y = y, group = group)
 
-    add_data!(v, x = x, y = y, group = group)
-    add_area!(v)
-
-	  v.marks[1].properties.enter.y2 = VegaValueRef(scale = "y", value = 0)
-	  v.marks[1].properties.enter.interpolate = VegaValueRef(scale = "y", value = "monotone")
-    v.marks[1].properties.enter.fill = VegaValueRef(scale = "group", field = "data.group")
+    v.marks[1]._type = "area"
+    v.marks[1].properties.enter.width = nothing
+    v.marks[1].properties.enter.interpolate = VegaValueRef(value = "monotone")
+    v.scales[1]._type = nothing
+    v.scales[1].zero = false
 
     return v
 end
--- src/derived/barplot.jl
+++ src/derived/barplot.jl
@@ -1,7 +1,9 @@
 function barplot(;x::AbstractVector = Int[],
                   y::AbstractVector = Int[],
                   y2::AbstractVector = Int[],
-                  group::AbstractVector = Int[])
+                  group::AbstractVector = Int[],
+                  stacked::Bool = false)
+
     v = VegaVisualization()
 
     default_scales!(v)
@@ -15,6 +17,33 @@ function barplot(;x::AbstractVector = Int[],
     v.marks[1].properties.enter.width = VegaValueRef(scale = "x", band = true, offset = -1)
     v.marks[1].properties.enter.y2 = VegaValueRef(scale = "y", field = "data.y2")
     v.marks[1].properties.enter.fill = VegaValueRef(scale = "group", field = "data.group")
+
+    if stacked
+      push!(v.data, VegaData(name = "stats", source = "table",
+                             transform = [VegaTransform(Dict{Any, Any}("type" => "facet", "keys" => ["data.x"])), VegaTransform(Dict{Any, Any}("type" => "stats", "value" => "data.y"))]))
+
+      v.scales[2].domain = VegaDataRef("stats", "sum")
+      v.scales[3].domain = nothing
+
+      v.marks[1].properties = nothing
+      v.marks[1]._type = "group"
+
+      v.marks[1].from = Dict{Any, Any}("data" => "table",
+                                       "transform" => [VegaTransform(Dict{Any, Any}("type" => "facet", "keys" => ["data.group"])), VegaTransform(Dict{Any, Any}("type" => "stack", "point" => "data.x", "height" => "data.y"))])
+
+      v.marks[1].marks = [
+                          VegaMark(_type = "rect",
+                          properties = VegaMarkProperties(
+                                                      enter = VegaMarkPropertySet(x = VegaValueRef(scale = "x", field = "data.x"),
+                                                                                  width = VegaValueRef(scale = "x", band = true, offset = -1),
+                                                                                  y = VegaValueRef(scale = "y", field = "y"),
+                                                                                  y2 = VegaValueRef(scale = "y", field = "y2"),
+                                                                                  fill = VegaValueRef(scale = "group", field = "data.group")
+                                                                                  )
+                                                          )
+                                  )
+                          ]
+    end
 
     return v
 end
