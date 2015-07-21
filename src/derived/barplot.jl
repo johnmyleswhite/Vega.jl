87ea8d67239e409c5e2c57e162c6d78b56a1a64c
Spencer Lyon <spencerlyon2@gmail.com>
2015-07-20 20:41:07 -0400

    ENH: barplot for vega2

--- src/derived/barplot.jl
+++ src/derived/barplot.jl
@@ -16,34 +16,20 @@
     add_rects!(v)
 
     v.marks[1].properties.enter.width = VegaValueRef(scale = "x", band = true, offset = -1)
+    v.marks[1].properties.enter.y = VegaValueRef(scale = "y", field = "y")
     v.marks[1].properties.enter.y2 = VegaValueRef(scale = "y", field = "y2")
     v.marks[1].properties.enter.fill = VegaValueRef(scale = "group", field = "group")
 
     if stacked
       push!(v.data, VegaData(name = "stats", source = "table",
-                             transform = [VegaTransform(Dict{Any, Any}("type" => "facet", "groupby" => ["x"])), VegaTransform(Dict{Any, Any}("type" => "stats", "value" => "y"))]))
+                             transform = [VegaTransform(Dict{Any, Any}("type"=> "aggregate", "groupby" => ["x"], "summarize"=>[Dict{Any,Any}("field" => "y", "ops" => ["sum"])]))]))
+      v.marks[1].properties.enter.y = VegaValueRef(scale = "y", field = "layout_start")
+    v.marks[1].properties.enter.y2 = VegaValueRef(scale = "y", field = "layout_end")
 
-      v.scales[2].domain = VegaDataRef("stats", "sum")
-      v.scales[3].domain = nothing
-
-      v.marks[1].properties = nothing
-      v.marks[1]._type = "group"
+      v.scales[2].domain = VegaDataRef("stats", "sum_y")
 
       v.marks[1].from = Dict{Any, Any}("data" => "table",
-                                       "transform" => [VegaTransform(Dict{Any, Any}("type" => "facet", "groupby" => ["group"])), VegaTransform(Dict{Any, Any}("type" => "stack", "point" => "x", "height" => "y"))])
-
-      v.marks[1].marks = [
-                          VegaMark(_type = "rect",
-                          properties = VegaMarkProperties(
-                                                      enter = VegaMarkPropertySet(x = VegaValueRef(scale = "x", field = "x"),
-                                                                                  width = VegaValueRef(scale = "x", band = true, offset = -1),
-                                                                                  y = VegaValueRef(scale = "y", field = "y"),
-                                                                                  y2 = VegaValueRef(scale = "y", field = "y2"),
-                                                                                  fill = VegaValueRef(scale = "group", field = "group")
-                                                                                  )
-                                                          )
-                                  )
-                          ]
+                                       "transform" => [VegaTransform(Dict{Any, Any}("type" => "stack", "groupby" => ["x"], "sortby" => ["group"], "field"=>"y"))])
     end
 
     #Return horizontal bar chart
