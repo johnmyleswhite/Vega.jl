ab8438171c12994923608c5e656a374e3cc823f1
Randy Zwitch <rzwitch@gmail.com>
2015-07-29 11:26:27 -0400

    Remove original vega folder
    Remove folder, see what breaks so we know what’s left to fix

--- deps/vega/src/parse/background.js
+++
@@ -1,6 +0,0 @@
-vg.parse.background = function(bg) {
-  // return null if input is null or undefined
-  if (bg == null) return null;
-  // run through d3 rgb to sanity check
-  return d3.rgb(bg) + "";
-};
--- deps/vega/src/parse/dataflow.js
+++
@@ -1,7 +0,0 @@
-vg.parse.dataflow = function(def) {
-  var tx = (def.transform || []).map(vg.parse.transform);
-  return !tx.length ? vg.identity :
-    function(data, db, group) {
-      return tx.reduce(function(d,t) { return t(d, db, group); }, data);
-    };
-};
