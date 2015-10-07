60b518eb3618252abfabb6a4b0bd6a10293390d0
Randy Zwitch <rzwitch@gmail.com>
2015-10-07 16:25:05 -0400

    Fixes #51
    Adds keywords for line plot, so that points can be added, the shape can
    be changed and the size can be changed

--- src/derived/lineplot.jl
+++ src/derived/lineplot.jl
@@ -1,7 +1,11 @@
 function lineplot(;x::AbstractVector = Int[],
                    y::AbstractVector = Int[],
-                   group::AbstractVector = Int[])
-    v = VegaVisualization()
+                   group::AbstractVector = Int[],
+                   points::Bool = false,
+                   pointShape::AbstractString = "circle",
+                   pointSize::Real = 10)
+
+    v = VegaVisualization(name = "lineplot")
 
     default_scales!(v)
     default_axes!(v)
@@ -18,6 +22,17 @@ function lineplot(;x::AbstractVector = Int[],
 
     #Make line width a little thicker
     v.marks[1].marks[1].properties.enter.strokeWidth = VegaValueRef(value = 2)
+
+    #Add points if desired
+    if points
+
+        copymark = deepcopy(v.marks[1].marks[1])
+        push!(v.marks[1].marks, copymark)
+        v.marks[1].marks[2]._type = "symbol"
+        v.marks[1].marks[2].properties.enter.size = VegaValueRef(value = pointSize)
+        v.marks[1].marks[2].properties.enter.shape = VegaValueRef(value = pointShape)
+
+    end
 
     #Default to Paired color scale, 12
     colorscheme!(v; palette = ("Paired", 12))
