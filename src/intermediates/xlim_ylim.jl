f34b701ff31c331682f570d628e326311e20996d
randyzwitch <rzwitch@gmail.com>
2015-11-28 19:35:21 -0500

    Fixes #85
    This fix should allow for user to change scale type, to provide
    log/pow/sqrt/ordinal etc.

--- src/intermediates/xlim_ylim.jl
+++ src/intermediates/xlim_ylim.jl
@@ -4,7 +4,7 @@ for fun in [:x, :y]
 
     fname = symbol("$(fun)lim!")
 
-    @eval begin function ($fname)(v::VegaVisualization; min::Real = 0, max::Real = 1000, reverse::Bool = false, round::Bool = false)
+    @eval begin function ($fname)(v::VegaVisualization; min::Real = 0, max::Real = 1000, reverse::Bool = false, round::Bool = false, _type::AbstractString = "", exponent::Real = 0.3)
 
                     if min > max
                         error("Min must be less than Max")
@@ -16,6 +16,11 @@ for fun in [:x, :y]
                     s.reverse = reverse
                     s.round = round
                     min != 0? s.zero = false : s.zero = true
+                    
+                    #Allow for changing scale type; if type == pow, also need to set exponent. Defaulting to 0.3 instead of d3 value of 1, since 1 returns identity
+                    #Returning identity is safer, but also makes user believe function is broken
+                    _type != ""? s._type = _type: nothing
+                    _type == "pow"? s.exponent = exponent: nothing
                     return v
                 end
           end
