b8edbb52fc24692d8c8693eb4d72c1c519751a9b
Lanfeng Pan <panlanfeng@gmail.com>
2016-07-24 17:10:01 -0500

    fix historam

--- src/derived/histogram.jl
+++ src/derived/histogram.jl
@@ -1,19 +1,70 @@
 function histogram(; x::AbstractVector = Int[],
-					relativefreq::Bool = false,
-					horizontal::Bool = false,
-					nbins::Int = 10)
+                    relativefreq::Bool = false,
+                    horizontal::Bool = false,
+                    nbins::Int = 10,
+                    bandwidth::Real=0.0)
 
-	a, b = hist(x, nbins)
-	total = relativefreq == false? 1 : sum(b)
+    if bandwidth == 0.0
+        a, b = hist(x, nbins)
+        bandwidth=a.step/a.divisor
+    else
+        xmin, xmax = extrema(x)
+        a, b = hist(x, xmin:bandwidth:xmax)
+    end
+    total = relativefreq ? sum(b) : 1
 
-    v = barplot(x = a[2:end], y = b/total)
+    #v = barplot(x = a[2:end], y = b/total)
+    v = VegaVisualization()
+    add_data!(v, x = a[1:end-1], x2=a[2:end],
+        y = b/total/bandwidth,y2=zeros(length(b)))
+    default_scales!(v; typeXaxis = "linear")
+    default_axes!(v)
 
-    #Remove space between bars per theory
-    v.marks[1].properties.enter.width.offset = 0
+    #Get unique table name
+    table = v.data[1].name
 
+    #old add_rects code
+    res = VegaMark(_type = "rect",
+        from = VegaMarkFrom(data=table),
+        properties = VegaMarkProperties(enter =
+            VegaMarkPropertySet(x = VegaValueRef(scale = "x", field = "x"),
+                y = VegaValueRef(scale = "y", field = "y"))))
+
+    v.marks == nothing? v.marks = [res] : push!(v.marks, res)
+    #end add_rects code
+
+    v.marks[1].properties.enter.x2 = VegaValueRef(scale = "x", field = "x2")
+    v.marks[1].properties.enter.y = VegaValueRef(scale = "y", field = "y")
+    v.marks[1].properties.enter.y2 = VegaValueRef(scale = "y", field = "y2")
+    #v.marks[1].properties.enter.fill = VegaValueRef(scale = "group",
+    #    field = "group")
+    v.marks[1].properties.enter.stroke = VegaValueRef(value="black")
+    v.marks[1].properties.enter.strokeWidth = VegaValueRef(value=0.25)
+
+    v.axes[findfirst([z._type == "x" for z in v.axes])].title = "x"
+    v.axes[findfirst([z._type == "y" for z in v.axes])].title =
+        relativefreq ? "density" : "counts"
+
+
+    #Return horizontal bar chart
     if horizontal
-        coord_flip!(v)
-        v.marks[1].properties.enter.height.offset = 0
+        #Switch scales
+        v.scales[findfirst([z.name == "x" for z in v.scales])].range = "height"
+        v.scales[findfirst([z.name == "y" for z in v.scales])].range = "width"
+
+        #Switch data
+        v.marks[1].properties.enter.x = VegaValueRef(scale = "y", field = "y2")
+        v.marks[1].properties.enter.x2 = VegaValueRef(scale = "y", field = "y")
+        v.marks[1].properties.enter.y = VegaValueRef(scale = "x", field = "x")
+        v.marks[1].properties.enter.y2 = VegaValueRef(scale = "x", field = "x2")
+
+        #Switch axes and title names
+        v.axes[findfirst([z._type == "x" for z in v.axes])].scale = "y"
+        v.axes[findfirst([z._type == "x" for z in v.axes])].title =
+            relativefreq ? "density" : "counts"
+        v.axes[findfirst([z._type == "y" for z in v.axes])].scale = "x"
+        v.axes[findfirst([z._type == "y" for z in v.axes])].title = "x"
+
     end
 
     return v
