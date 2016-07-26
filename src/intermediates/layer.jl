f92d91d836b6b6a77fb77df0549c68721986a43d
Lanfeng Pan <panlanfeng@gmail.com>
2016-07-26 15:19:34 -0500

    make multiple layer work

--- src/intermediates/layer.jl
+++ src/intermediates/layer.jl
@@ -9,19 +9,19 @@ function layer(plot1::VegaVisualization, plot2::VegaVisualization)
     #This allows for different colors for every layer, presuming you set the color prior to layering
     #Still need to write code to avoid overwriting value if it's already there
     i = findfirst([z.name == "group" for z in overlay.scales])
-    for m in length(overlay.marks)
+    for m in 1:length(overlay.marks)
         overlay.marks[m].scales = [overlay.scales[i]]
     end
 
     #Iterate over data tables in second plot, append to baseplot
     #This should be fine as-is
-    for i in length(overlay.data)
+    for i in 1:length(overlay.data)
         push!(baseplot.data, overlay.data[i])
     end
 
     #Add all marks to baseplot
     #This should be fine as-is
-    for i in length(overlay.marks)
+    for i in 1:length(overlay.marks)
         push!(baseplot.marks, overlay.marks[i])
     end
 
@@ -32,8 +32,32 @@ function layer(plot1::VegaVisualization, plot2::VegaVisualization)
     yloc2 = findfirst([z.name == "y" for z in overlay.scales])
 
     #Overwrite baseplot domain value
-    baseplot.scales[xloc1].domain = VegaDataRef(fields = [baseplot.scales[xloc1].domain, overlay.scales[xloc2].domain])
-    baseplot.scales[yloc1].domain = VegaDataRef(fields = [baseplot.scales[yloc1].domain, overlay.scales[yloc2].domain])
+    if baseplot.scales[xloc1].domain.fields == nothing
+        xdomain1=baseplot.scales[xloc1].domain
+    else
+        xdomain1=baseplot.scales[xloc1].domain.fields
+    end
+    if overlay.scales[xloc2].domain.fields == nothing
+        xdomain2=overlay.scales[xloc2].domain
+    else
+        xdomain2=overlay.scales[xloc2].domain.fields
+    end
+
+    if baseplot.scales[yloc1].domain.fields == nothing
+        ydomain1=baseplot.scales[yloc1].domain
+    else
+        ydomain1=baseplot.scales[yloc1].domain.fields
+    end
+    if overlay.scales[yloc2].domain.fields == nothing
+        ydomain2=overlay.scales[yloc2].domain
+    else
+        ydomain2=overlay.scales[yloc2].domain.fields
+    end
+
+    baseplot.scales[xloc1].domain = VegaDataRef(fields =
+        [xdomain1, xdomain2;])
+    baseplot.scales[yloc1].domain = VegaDataRef(fields =
+        [ydomain1, ydomain2;])
 
     return baseplot
 
